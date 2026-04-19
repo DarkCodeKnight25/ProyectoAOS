e04cd77eca44e90bf840b36ab3f377e6368abc95
DarkCodeKnight25 <darkcodeknight25@gmail.com>
2026-04-18 21:15:39 -0500

    archivo_bpmn

--- servicio-pedidos/servicio-pedidos/src/main/java/pe/edu/idat/pedidos/PedidoNoValidoException.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.idat.pedidos;
-
-
-
-public class PedidoNoValidoException extends RuntimeException {
-    public PedidoNoValidoException(String message) {
-        super(message);
-    }
-}
